a8e0de18d71126bfddcf3f435bd95ea448edc007
arvindd <1006084+arvindd@users.noreply.github.com>
2021-05-11 11:26:40 +0530

    Install bug fix.

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,7 +18,7 @@ scripts=(
 )
 
 # We need docker for our work. If docker not present, just inform and fail
-docker -v 2>1 > /dev/null
+docker -v 2>&1 > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "Docker not present, install docker before proceeding."
     echo "Go here to get docker: https://www.docker.com/"
@@ -33,19 +33,20 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Check if we have wget or curl
-wget --version 2>1 > /dev/null && WGET=1 || WGET=0
-curl --version 2>1 > /dev/null && CURL=1 || CURL=0
+wget --version 2>&1 > /dev/null && WGET=1 || WGET=0
+curl --version 2>&1 > /dev/null && CURL=1 || CURL=0
 
 # Create a devlab directory where we will pull the scripts
 mkdir -p devlab
+echo "${scripts[@]}"
 
 if [[ ${WGET} -eq 1 ]]; then
-    for s in ${script[@]} do
+    for s in "${scripts[@]}"; do
         wget $s -O devlab/`basename $s`
         chmod +x devlab/`basename $s`
     done
 elif [[ ${CURL} -eq 1 ]]; then
-    for s in ${script[@]} do
+    for s in "${scripts[@]}"; do
         curl $s -o devlab/`basename $s`
         chmod +x devlab/`basename $s`        
     done
